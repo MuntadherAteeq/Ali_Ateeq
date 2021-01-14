3daf590eb432486cafbf51a01bc2ec9626e58c23
mr-piyson <manmoon619@gmail.com>
2021-01-14 15:48:39 +0300

    I Create Transactions Activity with Flexable Toolbar And linked the Recycle view with Transactions class

--- app/src/main/java/com/example/ali_ateeq/Transactions.java
+++ app/src/main/java/com/example/ali_ateeq/Transactions.java
@@ -1,8 +1,11 @@
 package com.example.ali_ateeq;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.rhexgomez.typer.roboto.TyperRoboto;
 
 public class Transactions extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Transactions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
+
+        CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsing_toolbar);
+       // collapsingToolbar.setCollapsedTitleTypeface(TyperRoboto.ROBOTO_REGULAR());
+        // collapsingToolbar.setExpandedTitleTypeface(TyperRoboto.ROBOTO_REGULAR());
+        collapsingToolbar.setTitle("Ali ATEEQ");
+        collapsingToolbar.setCollapsedTitleTextColor(Color.rgb(255, 255, 255));
+
+
     }
 }
